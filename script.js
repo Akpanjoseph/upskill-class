cbb778d3ccd123a47a7ec2c3a579a9241678c37d
Syntax Designer <syntaxdesigners@gmail.com>
2023-06-10 21:28:59 +0100

    introducing js

---
+++ script.js
@@ -0,0 +1,72 @@
+//console.log("Hello world ")
+
+// single line comment
+
+//displaying a text 
+console.log("how are you")
+
+// multi line comment
+/** 
+console.log("fine")
+console.log("Hello")
+console.log("hi")
+
+*/
+
+
+
+/** 
+ * 
+ * variable is a container that can hold any possiable value
+ *  var - loose way of creating a variable 
+ * let -   
+ * const -
+ * 
+ * keyword (var,let,const) variable name = value
+ * 
+ * version  - ecma script    es6 and es5
+ * 
+ * Data types - 
+ *  string - abc  || " any here"    ' here' " 23445"
+ *  number 0.1 - 1 - 2 billon
+ *  null - empty
+ *  undefined - assigned variable value
+ *  object - key and values
+ *  boolean - true or false 
+ *  
+ * String name = "hello"
+ * 
+ * naming convention
+ * 1. cermal case
+ * 2. under score 
+ * school name 
+ * 
+ * Check data type
+ * typeof()
+ * 
+ * 
+ */
+
+
+// var text1 = "text1";
+let text2 = "text2";
+const text3 = " text3"
+
+// let text2 = "jdhd"
+
+let name;
+let age = 30
+name = "syntax"
+
+// cermal
+let schoolAddress = "b1" 
+
+// under score
+let my_name = "syntax"
+
+let the_name_of_my_school
+
+console.log(typeof(age))
+console.log(typeof(name))
+
+
